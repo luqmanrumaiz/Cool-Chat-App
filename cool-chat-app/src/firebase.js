7eb3767d591d0b6ccfa96b640cd532977d073656
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-06-08 21:34:25 +0530

    Added Firebase Config File

---
+++ cool-chat-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDRJdI3iqyMbW92EwXpKa96CYcMJkhy8Q",
+  authDomain: "cool-chat-app-5b87d.firebaseapp.com",
+  projectId: "cool-chat-app-5b87d",
+  storageBucket: "cool-chat-app-5b87d.appspot.com",
+  messagingSenderId: "897553106140",
+  appId: "1:897553106140:web:aef80aa0a3fbaed763e019",
+  measurementId: "G-HCZH852M1R",
+};

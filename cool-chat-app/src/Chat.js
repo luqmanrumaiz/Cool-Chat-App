baed6d2c536cb3e03e291fc0eb866c2296766fae
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-06-09 14:04:28 +0530

    Completed Chat Input Bar

--- cool-chat-app/src/Chat.js
+++ cool-chat-app/src/Chat.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Chat.css";
-import { Center, Flex, HStack, Spacer, Text } from "@chakra-ui/layout";
+import { Center, Flex, HStack, Spacer, Stack, Text } from "@chakra-ui/layout";
 import { Avatar, Box, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import { Tag, TagLabel } from "@chakra-ui/tag";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/input";
+import SendIcon from "@material-ui/icons/Send";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 
 function Chat(props) {
+  const [value, setValue] = React.useState("");
+  const handleChange = (event) => setValue(event.target.value);
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -35,28 +46,74 @@ function Chat(props) {
       </div>
 
       <div className="chat__body">
-        <Tag
-          padding="10px"
-          size="lg"
-          colorScheme="telegram"
-          borderRadius="full"
-        >
-          <Avatar
-            src="https://bit.ly/sage-adebayo"
-            size="xs"
-            name="Segun Adebayo"
-            ml={-1}
-            mr={2}
-          />
-          <TagLabel className="chat__message">
-            Hey..
-            <span className="chat__timestamp">
-              {" "}
-              {new Date().toUTCString()}{" "}
-            </span>{" "}
-          </TagLabel>
-        </Tag>
+        <Stack>
+          <Tag
+            padding="10px"
+            size="md"
+            colorScheme="telegram"
+            borderRadius="full"
+          >
+            <Avatar
+              src="https://bit.ly/sage-adebayo"
+              size="xs"
+              name="Segun Adebayo"
+              ml={-1}
+              mr={2}
+            />
+            <TagLabel className="chat__message">
+              Hey..
+              <span className="chat__timestamp">
+                {new Date().toUTCString()}
+              </span>
+            </TagLabel>
+          </Tag>
+          <Tag
+            padding="10px"
+            size="md"
+            colorScheme="whatsapp"
+            borderRadius="full"
+          >
+            <Avatar
+              src="https://avatars.githubusercontent.com/u/61153637?s=400&u=6470fc8cfab0bddcd486a30beb4d4e5a67660c6a&v=4"
+              size="xs"
+              name="Segun Adebayo"
+              ml={-1}
+              mr={2}
+            />
+            <TagLabel className="chat__message">
+              No I do not want to talk
+              <span className="chat__timestamp">
+                {new Date().toUTCString()}
+              </span>
+            </TagLabel>
+          </Tag>
+        </Stack>
       </div>
+      <InputGroup className="chat__input">
+        <InputLeftElement
+          color="gray.300"
+          fontSize="0.8em"
+          children={
+            <IconButton>
+              <EmojiEmotionsIcon style={{ color: "lightgrey" }} />
+            </IconButton>
+          }
+        />
+        <Input
+          variant="flushed"
+          value={value}
+          fontSize="0.8em"
+          onChange={handleChange}
+          placeholder="Enter Message"
+        />
+        <InputRightElement
+          children={
+            <IconButton>
+              <SendIcon style={{ color: "lightgrey" }} />
+            </IconButton>
+          }
+        />
+      </InputGroup>
     </div>
   );
 }
